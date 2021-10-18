eaf8d0a5f02ee9b7255ec794ca4863f480276b38
Sewef <elagroum@gmail.com>
2021-10-18 15:58:27 +0200

    Better bold, clickable blocks, working titles

---
+++ posts/posts.js
@@ -0,0 +1,5 @@
+var posts = [
+	{ path: "20211018.003", 	title: "dummy3"},
+	{ path: "20211018.002", 	title: "dummy2"},
+	{ path: "20211018.001", 	title: "dummy1"},
+];
--- scripts/printUI.js
+++ scripts/printUI.js
@@ -1,8 +1,17 @@
 function draw()
 {
-	for (let i=0; i<50; i++)
-	{
-		document.getElementsByClassName("blocks")[0].innerHTML += "<div>a</div>";
-		console.log("draw "+i);
-	}
+	posts.forEach(function(item)
+	  {
+		console.log(item.path);
+		document.getElementsByClassName("blocks")[0].innerHTML += `<a href=\"/posts/${item.path}\"><div>${item.title}</div></a>`;
+		
+		
+	  }
+	);
+
+	// for (let i=0; i<50; i++)
+	// {
+		// document.getElementsByClassName("blocks")[0].innerHTML += "<div>AzerTy</div>";
+		// console.log("draw "+i);
+	// }
 }

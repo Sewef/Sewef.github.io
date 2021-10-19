dadba792dde176b56715bdaef1f2b79898ea98d2
Sewef <elagroum@gmail.com>
2021-10-19 09:36:06 +0200

    Tumbnails & pin post

--- posts/posts.js
+++ posts/posts.js
@@ -1,5 +1,5 @@
 var posts = [
+	{ path: "20211018.001", 	title: "📌 Hello world!"},
 	{ path: "20211018.003", 	title: "dummy3"},
 	{ path: "20211018.002", 	title: "dummy2"},
-	{ path: "20211018.001", 	title: "dummy1"},
 ];
--- scripts/printUI.js
+++ scripts/printUI.js
@@ -3,9 +3,13 @@ function draw()
 	posts.forEach(function(item)
 	  {
 		console.log(item.path);
-		document.getElementsByClassName("blocks")[0].innerHTML += `<a href=\"/posts/${item.path}\"><div>${item.title}</div></a>`;
-		
-		
+		let imgStr = `<img src=\"/posts/${item.path}/tumbnail.png\">`;
+		document.getElementsByClassName("blocks")[0].innerHTML += 
+			`<a href=\"/posts/${item.path}\"  style=\"background-image:url(/posts/${item.path}/tumbnail.jpg)\">
+			<div>
+			${item.title}
+			</div>
+			</a>`;
 	  }
 	);
 

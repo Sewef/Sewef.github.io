afbeacbb8dba9e132f287c8de22cd5e39718ae12
Sewef <elagroum@gmail.com>
2021-10-22 10:14:00 +0200

    Cleanup & motd

--- scripts/printUI.js
+++ scripts/printUI.js
@@ -1,21 +1,18 @@
-function draw()
-{
-	posts.forEach(function(item)
-	  {
-		console.log(item.path);
-		let imgStr = `<img src=\"/posts/${item.path}/tumbnail.png\">`;
-		document.getElementsByClassName("blocks")[0].innerHTML += 
-			`<a href=\"/posts/${item.path}\"  style=\"background-image:url(/posts/${item.path}/tumbnail.jpg)\">
-			<div>
-			${item.title}
-			</div>
-			</a>`;
-	  }
-	);
+posts.forEach(function(item)
+  {
+	console.log(item.path);
+	let imgStr = `<img src=\"/posts/${item.path}/tumbnail.png\">`;
+	document.getElementsByClassName("blocks")[0].innerHTML += 
+		`<a href=\"/posts/${item.path}\"  style=\"background-image:url(/posts/${item.path}/tumbnail.jpg)\">
+		<div>
+		${item.title}
+		</div>
+		</a>`;
+  }
+);
 
-	// for (let i=0; i<50; i++)
-	// {
-		// document.getElementsByClassName("blocks")[0].innerHTML += "<div>AzerTy</div>";
-		// console.log("draw "+i);
-	// }
-}
+// for (let i=0; i<50; i++)
+// {
+	// document.getElementsByClassName("blocks")[0].innerHTML += "<div>AzerTy</div>";
+	// console.log("draw "+i);
+// }

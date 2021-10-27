699f6e234386141a11f7277254a68502076f284a
Sewef <elagroum@gmail.com>
2021-10-27 12:37:58 +0200

    Languages
    - Detect browser lang
    - Links keep lang
    - Logs cleanup

--- scripts/printUI.js
+++ scripts/printUI.js
@@ -1,6 +1,5 @@
 posts.forEach(function(item)
   {
-	console.log(item.path);
 	let imgStr = `<img src=\"/posts/${item.path}/tumbnail.png\">`;
 	document.getElementsByClassName("blocks")[0].innerHTML += 
 		`<a href=\"/posts/${item.path}\"  style=\"background-image:url(/posts/${item.path}/tumbnail.jpg)\">
--- scripts/setLanguage.js
+++ scripts/setLanguage.js
@@ -1,18 +1,37 @@
+// Detect page language depending on url params
+
 let params = (new URL(document.location)).searchParams;
 
 if (params.get("lang") == null)
 {
+	let userLang = navigator.language || navigator.userLanguage; 
 	let url = (new URL(document.location));
-	url.searchParams.set("lang", "fr");
+	if (userLang == "fr")
+		url.searchParams.set("lang", "fr");
+	else
+		url.searchParams.set("lang", "en");
 	document.location = url;
 }
 
-if (params.get("lang") != null)
-{
-	let stylesheet = getStyleSheet("default");
-	let lang = params.get("lang");
-	stylesheet.insertRule(`*[lang][lang=${lang}] { display: initial; }`, 0);
-}
+let stylesheet = getStyleSheet("default");
+var lang = params.get("lang");
+stylesheet.insertRule(`*[lang][lang=${lang}] { display: initial; }`, 0);
+
+// Change link on click
+document.addEventListener("click", 
+	function(event)
+	{
+		let target = event.target;
+		if (target.tagName == "A")
+		{
+			let url = new URL(target.href);
+			url.searchParams.set("lang", lang);
+			target.href = url.toString();
+		}
+	}
+);
+
+// Change language using menu
 
 function changeLanguage(lang)
 {
@@ -20,6 +39,8 @@ function changeLanguage(lang)
 	url.searchParams.set("lang", lang);
 	document.location = url;
 }
+
+// Utils
 
 function getStyleSheet(unique_title) {
 	for(var i=0; i<document.styleSheets.length; i++)

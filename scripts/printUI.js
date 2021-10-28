20c88f7b5bd252a6d0ee3ecf97297b32e968ca30
Sewef <elagroum@gmail.com>
2021-10-28 09:16:59 +0200

    Relative path for UI print

--- scripts/printUI.js
+++ scripts/printUI.js
@@ -1,11 +1,11 @@
 posts.forEach(function(item)
-  {
-	let imgStr = `<img src=\"/posts/${item.path}/tumbnail.png\">`;
-	document.getElementsByClassName("blocks")[0].innerHTML += 
-		`<a href=\"/posts/${item.path}\"  style=\"background-image:url(/posts/${item.path}/tumbnail.jpg)\">
-		<div>
-		${item.title}
-		</div>
-		</a>`;
-  }
+	{
+		let imgStr = `<img src="${window.location.pathname}posts/${item.path}/tumbnail.png">`;
+		document.getElementsByClassName("blocks")[0].innerHTML += 
+			`<a href=\"${window.location.pathname}posts/${item.path}\"  style=\"background-image:url(${window.location.pathname}posts/${item.path}/tumbnail.jpg)\">
+			<div>
+			${item.title}
+			</div>
+			</a>`;
+	}
 );

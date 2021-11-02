7a10444961ecc89208371ccffc967a7cb326dc75
Sewef <elagroum@gmail.com>
2021-11-02 09:39:40 +0100

    tumbnail → thumbnail

--- scripts/printUI.js
+++ scripts/printUI.js
@@ -1,8 +1,8 @@
 posts.forEach(function(item)
 	{
-		let imgStr = `<img src="${window.location.pathname}posts/${item.path}/tumbnail.png">`;
+		let imgStr = `<img src="${window.location.pathname}posts/${item.path}/thumbnail.png">`;
 		document.getElementsByClassName("blocks")[0].innerHTML += 
-			`<a href=\"${window.location.pathname}posts/${item.path}\"  style=\"background-image:url(${window.location.pathname}posts/${item.path}/tumbnail.jpg)\">
+			`<a href=\"${window.location.pathname}posts/${item.path}\"  style=\"background-image:url(${window.location.pathname}posts/${item.path}/thumbnail.jpg)\">
 			<div>
 			${item.title}
 			</div>

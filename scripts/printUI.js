9f6c014cfc1de9fb73270950732aa016f58dc135
Sewef <elagroum@gmail.com>
2021-11-05 09:01:33 +0100

    blocks is an Id ; remove unused var

--- scripts/printUI.js
+++ scripts/printUI.js
@@ -1,7 +1,6 @@
 posts.forEach(function(item)
 	{
-		let imgStr = `<img src="${window.location.pathname}posts/${item.path}/thumbnail.png">`;
-		document.getElementsByClassName("blocks")[0].innerHTML += 
+		document.getElementById("blocks").innerHTML += 
 			`<a href=\"${window.location.pathname}posts/${item.path}\"  style=\"background-image:url(${window.location.pathname}posts/${item.path}/thumbnail.jpg)\">
 			<div>
 			${item.title}

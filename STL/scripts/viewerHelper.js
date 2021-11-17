4aba355ed4a9022635cd0d29b76b773c4f21db93
Sewef <elagroum@gmail.com>
2021-11-17 11:28:34 +0100

    readability changes

--- STL/scripts/viewerHelper.js
+++ STL/scripts/viewerHelper.js
@@ -8,20 +8,23 @@ files.forEach(
 	{
 		// console.log(item);
 		list.innerHTML += `<a href="javascript:void(0)" onclick="setView(this)">
-		${(/(?:\.([^.]+))?$/).exec(item)[1] == "stl" ? "📦" : "🖼️" }	${item}</a>`;
+		${getExtension(item) == "stl" ? "📦" : "🖼️" }	${item}</a>`;
 	}
 );
 list.firstChild.click();
 
 // Actions on clicking file
-
 function setView(item)
 {
-	if (document.querySelector('[selected]') != null) document.querySelector('[selected]').removeAttribute('selected');
+	if (document.querySelector('[selected]') != null)
+		document.querySelector('[selected]').removeAttribute('selected');
 	
 	item.setAttribute('selected', '');
-	var text = (/^.*\s(.*)/).exec(item.innerText)[1]; //Trim the icon
-	if ((/(?:\.([^.]+))?$/).exec(text)[1] == "stl") //Get extension
+	
+	let text = item.innerText.replace(/[\W_]+/, ''); //Trim the icon
+	let extension = getExtension(text)
+	
+	if (extension == "stl") //Get extension
 	{
 		if (stl_viewer == null)
 		{
@@ -36,26 +39,39 @@ function setView(item)
 		}
 		
 		document.getElementById("display").style.backgroundImage = "";
-		if (stl_viewer.models_count == 0 || stl_viewer.get_model_info(0).name != `${window.location.pathname}${text}`)
+		if (stl_viewer.models_count == 0 ||
+		stl_viewer.get_model_info(0).name != `${window.location.pathname}${text}`)
 		{
 			stl_viewer.clean();
-			stl_viewer.add_model({id:0, filename:`${window.location.pathname}${text}`, color:"#008FFF", rotationx: -Math.PI/2});
+			stl_viewer.add_model({
+				id:0,
+				filename:`${window.location.pathname}${text}`,
+				color:"#008FFF",
+				rotationx: -Math.PI/2
+				});
 		}
 	}
-	else
+	else if (extension == "jpg" || extension == "png")
 	{
-		if (stl_viewer != null) stl_viewer.clean();
+		if (stl_viewer != null)
+			stl_viewer.clean();
 		document.getElementById("display").style.backgroundImage = `url(${window.location.pathname}${text})`;
 	}
 }
 
 // Export PNG
-
-function exportPng()
+function exportImage()
 {
-	var text = (/^.*\s(.*)/).exec(document.querySelector('[selected]').innerText)[1]; //Trim the icon
-	if ((/(?:\.([^.]+))?$/).exec(text)[1] == "stl") //Get extension
+	let text = item.innerText.replace(/[\W_]+/, ''); //Trim the icon
+	let extension = getExtension(text)
+	if (extension == "stl") //Get extension
 		window.open(document.getElementsByTagName("canvas")[0].toDataURL("image/png")); // Export stl view
 	else
-		window.open(`${window.location.pathname}${text}`); // Show BG image
+		window.open(`${window.location.pathname}${text}`);
+}
+
+// Misc
+function getExtension(path)
+{
+	return (/(?:\.([^.]+))?$/).exec(path)[1];
 }

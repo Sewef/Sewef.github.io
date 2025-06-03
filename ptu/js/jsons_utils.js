396ec0b789a78d66820fb734899c500c30ec0c38
Sewef <elagroum@gmail.com>
2025-06-03 16:11:58 +0200

    bump

--- ptu/js/jsons_utils.js
+++ ptu/js/jsons_utils.js
@@ -28,7 +28,6 @@ function replaceStringsWithDictionary(inputString, dictionary) {
 
 
 function loadJsonAsCard(file, container) {
-    console.log(container);
     $.getJSON(file, function (data) {
         if (!Array.isArray(data) || data.length === 0) {
             alert(`Error: no data found in ${file}`);
@@ -44,16 +43,24 @@ function loadJsonAsCard(file, container) {
                 else if (key == "name") {
                     str += `<strong>${item[key] !== undefined ? item[key] : ""}</strong><br>`;
                 }
+                else if (key == "damage_base") {
+                    if (item[key] !== undefined) {
+                        const regex = /(Damage Base.*:) (.*)/;
+                        const match = item[key].match(regex);
+                        if (!match) {
+                            console.log("Error: damage_base format is incorrect in item:", item);
+                        }
+                        else {
+                            str += `<strong>${match[1]}</strong> ${match[2]}<br>`;
+                        }
+                    }
+                }
                 else {
                     const parsedKey = replaceStringsWithDictionary(key, dictionary);
                     str += `<strong>${parsedKey}</strong>: ${item[key] !== undefined ? item[key] : ""}<br>`;
                 }
             });
-            //str = str.slice(0, -2); // Remove the trailing comma and space
-            //console.log(str);
-
-            //container.innerHTML += `<div class=".col-4">${str}</div>`;
-
+            
             const cardDiv = document.createElement('div');
             cardDiv.className = "col-12 col-md-4";
             cardDiv.innerHTML = `<div class="card h-100"><div class="card-body">${str}</div></div>`;
